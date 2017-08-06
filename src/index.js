a4c4b1f506aba5769004f8fb3d3734ea4920a1c4
Michael Mathews <mmathews87@gmail.com>
2017-08-07 00:08:03 +0100

    jwt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
+const { SECRET } = process.env;
 const graphqlHTTP = require('express-graphql');
 const Schema = require('./schema');
+const { send, json } = require('micro');
+const jwtAuth = require('micro-jwt-auth');
 
-module.exports = graphqlHTTP({
-  schema: Schema,
-  graphiql: true,
-  pretty: true
-});
+module.exports = jwtAuth(SECRET)(
+  graphqlHTTP({
+    schema: Schema,
+    graphiql: true,
+    pretty: true
+  })
+);
